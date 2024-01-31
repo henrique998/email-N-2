85b6fa2247c009481a02b61a08ea04f50c307cbf
Henrique Monteiro <henriquemonteiro037@gmail.com>
2024-01-31 02:08:02 +0000

    .env.example added

--- internal/endpoints/auth.go
+++ internal/endpoints/auth.go
@@ -3,6 +3,7 @@ package endpoints
 import (
 	"context"
 	"net/http"
+	"os"
 	"strings"
 
 	oidc "github.com/coreos/go-oidc/v3/oidc"
@@ -20,7 +21,7 @@ func Auth(next http.Handler) http.Handler {
 		}
 
 		tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
-		provider, err := oidc.NewProvider(r.Context(), "https://8080-henrique998-emailn2-5wivhr38yuy.ws-us107.gitpod.io/realms/provider")
+		provider, err := oidc.NewProvider(r.Context(), os.Getenv("KEYCLOAK_URL"))
 		if err != nil {
 			render.Status(r, 500)
 			render.JSON(w, r, map[string]string{"error": "error to connect to provider"})
--- internal/infra/database/new_db.go
+++ internal/infra/database/new_db.go
@@ -1,13 +1,15 @@
 package database
 
 import (
+	"os"
+
 	campaing "github.com/henrique998/email-N/internal/domain/campaign"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewDb() *gorm.DB {
-	dsn := "host=localhost user=henrique password=123456 dbname=postgres port=5432 sslmode=disable"
+	dsn := os.Getenv("DATABASE_URL")
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
